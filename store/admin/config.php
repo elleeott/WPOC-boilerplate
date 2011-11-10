b566a3e94e142afa959d4701bc4e2ea6902785e4
Elliott Rounsavall <elliott.rounsavall@gmail.com>
2011-11-10 14:07:43 -0500

    are we there yet

---
+++ store/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://frictio.dev/store/admin/');
+define('HTTP_CATALOG', 'http://frictio.dev/store/');
+define('HTTP_IMAGE', 'http://frictio.dev/store/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://frictio.dev/store/admin/');
+define('HTTPS_IMAGE', 'http://frictio.dev/store/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/drive2/Dropbox/Sites/frictious/store/admin/');
+define('DIR_SYSTEM', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/');
+define('DIR_DATABASE', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/database/');
+define('DIR_LANGUAGE', '/Volumes/drive2/Dropbox/Sites/frictious/store/admin/language/');
+define('DIR_TEMPLATE', '/Volumes/drive2/Dropbox/Sites/frictious/store/admin/view/template/');
+define('DIR_CONFIG', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/config/');
+define('DIR_IMAGE', '/Volumes/drive2/Dropbox/Sites/frictious/store/image/');
+define('DIR_CACHE', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/drive2/Dropbox/Sites/frictious/store/download/');
+define('DIR_LOGS', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/logs/');
+define('DIR_CATALOG', '/Volumes/drive2/Dropbox/Sites/frictious/store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'elleeott_frictio');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'frictio');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ store/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://frictio.dev/store/');
+define('HTTP_IMAGE', 'http://frictio.dev/store/image/');
+define('HTTP_ADMIN', 'http://frictio.dev/store/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://frictio.dev/store/');
+define('HTTPS_IMAGE', 'http://frictio.dev/store/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/drive2/Dropbox/Sites/frictious/store/catalog/');
+define('DIR_SYSTEM', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/');
+define('DIR_DATABASE', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/database/');
+define('DIR_LANGUAGE', '/Volumes/drive2/Dropbox/Sites/frictious/store/catalog/language/');
+define('DIR_TEMPLATE', '/Volumes/drive2/Dropbox/Sites/frictious/store/catalog/view/theme/');
+define('DIR_CONFIG', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/config/');
+define('DIR_IMAGE', '/Volumes/drive2/Dropbox/Sites/frictious/store/image/');
+define('DIR_CACHE', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/drive2/Dropbox/Sites/frictious/store/download/');
+define('DIR_LOGS', '/Volumes/drive2/Dropbox/Sites/frictious/store/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'elleeott_frictio');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'frictio');
+define('DB_PREFIX', 'oc_');
+?>
