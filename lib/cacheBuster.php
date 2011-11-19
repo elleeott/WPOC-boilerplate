35b51485d959c4fa60c973e4eaf7376907394ed4
Elliott Rounsavall <elliott.rounsavall@gmail.com>
2011-11-19 15:57:11 -0500

    config tweaks

--- lib/cacheBuster.php
+++ lib/cacheBuster.php
@@ -3,6 +3,6 @@ function autoVer($url){
 	$subDomain ='static';
     $path = pathinfo($url);
     $ver = '.'.filemtime($_SERVER['DOCUMENT_ROOT'].$url).'.';
-    echo '//'.$subDomain.'.'.$_SERVER['HTTP_HOST'].str_replace($subDomain.'/','',$path['dirname']).'/'.str_replace('.', $ver, $path['basename']);
+    echo '//'.$subDomain.'.'.$_SERVER['SERVER_NAME'].str_replace($subDomain.'/','',$path['dirname']).'/'.str_replace('.', $ver, $path['basename']);
 }
 ?>
