9a728f9061f69a5a0e92900aebd69081027b94a5
elliott frictio <elliott.rounsavall@gmail.com>
2012-02-09 09:44:56 -0500

    resolving hosting issues

--- infamation.php
+++ infamation.php
@@ -2,4 +2,7 @@
 <?php echo $_SERVER['HTTP_HOST']; ?><br/>
 <?php echo $_SERVER['SERVER_NAME']; ?><br/>
 <?php echo exec('whoami'); ?><br/>
+<?php echo $_SERVER['DOCUMENT_ROOT'] . '/static/wp-content'; ?><br/>
+<?php echo '//static.'.str_replace('www.','',$_SERVER['SERVER_NAME']).'/wp-content'; ?>
+
 <?php phpinfo(); ?>
